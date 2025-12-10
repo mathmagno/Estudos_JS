752a8898ebb3c971c15dde7eb871ae3894c60cc9
math_magno <98839706+mathmagno@users.noreply.github.com>
2025-12-09 23:19:32 -0300

    Variáveis primitivas

---
+++ aula09/index.js
@@ -0,0 +1,14 @@
+const nome = "Matheus"; //String
+const nome1 = "Matheus"; //Sring
+const num1 = 10; //number
+const num2 = 10.50; // number float
+let nomeAluno; // undefined = não aponta para nenhum lugar da memória!
+let sobrenomeAluno = null; // Nulo -> não aponta pra local nenhuma na memória
+
+let a = [1, 2];
+const b = a;
+
+console.log(a, b);
+
+a = 3;
+console.log(a, b);
