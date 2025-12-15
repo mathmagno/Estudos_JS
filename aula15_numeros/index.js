d0404cb187772e90c991a09ae3332a5cc63b9727
math_magno <98839706+mathmagno@users.noreply.github.com>
2025-12-15 20:17:59 -0300

    add

---
+++ aula15_numeros/index.js
@@ -0,0 +1,9 @@
+let num1 = 10;
+let num2= 2.5;
+
+// console.log(num1.toString() + num2);
+// console.log(typeof num1);
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(4));
+let temp = num1 * 'Ola';
+console.log(Number.isNaN(temp));
---
+++ aula16_objetomath/index.js
@@ -0,0 +1,8 @@
+let num1 = 9.54578;
+console.log(typeof(num1 = Number))
+//console.log(num1 ** (1/2)); Raiz Quadrada!!!
+//let num2 = Math.floor(num1); Arradondar para baixo
+// let num2 = Math.ceil(num1); Arredondar para cima
+// let num2 = Math.round(num1);
+// console.log(Math.max(1,2,3,4,5,6));
+// console.log(Math.min(1,2,3,4,5,6));
