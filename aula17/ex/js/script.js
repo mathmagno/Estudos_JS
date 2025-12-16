c92e711405c5f482e00d08d8b8693bd3c90f5f25
math_magno <98839706+mathmagno@users.noreply.github.com>
2025-12-16 19:58:40 -0300

    add

--- aula17/ex/js/script.js
+++ aula17/ex/js/script.js
@@ -1,2 +1,13 @@
-const numero = prompt("Qual número?");
-document.getElementById()
+const numero = Number(prompt("Qual número?"));
+const numeroTitulo = document.getElementById('numero-titulo');
+const textoTitulo = document.getElementById('texto');
+
+numeroTitulo.innerHTML = numero;
+
+textoTitulo.innerHTML = '';
+textoTitulo.innerHTML += `<p><b>Seu número é ${numero}</b></p>`;
+textoTitulo.innerHTML += `<p>Raiz quadrada: ${numero ** 0.5}</p>`;
+textoTitulo.innerHTML += `<p>${numero} é número inteiro: ${Number.isInteger(numero)}.</p>`;
+textoTitulo.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}.</p>`;
+textoTitulo.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}.</p>`;
+textoTitulo.innerHTML += `<p>Com duas casa decimais: ${numero.toFixed(2)}.</p>`;
