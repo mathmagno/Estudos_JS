6b5223d828a30a138f3d7ec710dcf14d5fa3df78
math_magno <98839706+mathmagno@users.noreply.github.com>
2025-12-18 13:13:33 -0300

    estudo de arrays

---
+++ aula18_arrays/index.js
@@ -0,0 +1,30 @@
+const alunos = ['Matheus','João','Carlos'];
+
+
+// delete alunos[2];   SERVE PARA DELETAR E NÃO ALTERAR NA ESTRUTURA
+// console.log(alunos);
+
+//const removidos = alunos.shift(); REMOVER DADOS DO INÍCIO
+//console.log(removidos);
+//console.log(alunos);
+
+
+//const removidos = alunos.pop();  REMOVER DADOS NO ÚLTIMO LUGAR
+//console.log(removidos);
+//console.log(alunos);
+
+
+
+
+/* COMENTÁRIOS ABAIXO SÃO PARA ADIÇÃO DE DADOS */
+
+// alunos.unshift('Matheus M'); ADCIONAR EM PRIMEIRO 
+
+// alunos.push('Jéssica'); ADCIONAR DADO POR ÚLTIMO
+// alunos.push('Fernando');
+
+// alunos[alunos.length] = 'Jorge'; Adicionar no último número
+
+// alunos[1] = 'Andressa';  CASO EXISTA UM DADO SERÁ TROCADO, CASO NÃO EXISTIR, SERÁ ADICIONADO
+// alunos[4] = 'Jéssica';
+// alunos[3] = 'Fernanda';
