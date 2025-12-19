e0f100b3565ba5b55a1391e8169fbf56d5ea53fd
math_magno <98839706+mathmagno@users.noreply.github.com>
2025-12-19 14:37:21 -0300

    funcoes

---
+++ aula19_funcao/index.js
@@ -0,0 +1,12 @@
+
+
+
+
+//function soma(x, y){
+//    const resultado = x + y;
+//    return resultado; 
+//}
+//
+//const resultado = soma(4, 2);
+//console.log(resultado);
+//console.log(soma(2,2));
