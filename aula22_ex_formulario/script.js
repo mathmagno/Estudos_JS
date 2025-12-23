bf9e0ff7fa86b6e14318beb72730b1d78741bdb8
math_magno <98839706+mathmagno@users.noreply.github.com>
2025-12-23 15:31:09 -0300

    form

--- aula22_ex_formulario/script.js
+++ aula22_ex_formulario/script.js
@@ -1,21 +1,38 @@
 function meuEscopo () {
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado')
 
+    const pessoas = [];
     //form.onsubmit = function (evento) {
     //    evento.preventDefault();
     //    alert();   
     //    console.log('Foi enviado!');
     //};
 
-    let contador = 1;
     function recebeEventoForm (evento) {
-        evento.preventDefault();
-        console.log(`Form não foi enviado ${contador}`);
-        contador++;
+        evento.preventDefault(); // linha para prevenir evento
+
+        const nome = form.querySelector('.nome'); // .nome serve para buscar classe
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+        
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+        
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} tem o peso ${peso.value} e ${altura.value} de altura!</p>`
+        
     }
 
 
-    form.addEventListener('submit', recebeEventoForm );
+    form.addEventListener('submit', recebeEventoForm);
 }
 
 meuEscopo();
